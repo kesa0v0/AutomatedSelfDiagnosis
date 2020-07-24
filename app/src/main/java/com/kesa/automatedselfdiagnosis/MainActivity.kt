8cd18a08ae8522fc74fb1f39a4ef9df8e8a274fb
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-07-24 22:53:22 +0900

    Initialize TimePicker with previous settings

--- app/src/main/java/com/kesa/automatedselfdiagnosis/MainActivity.kt
+++ app/src/main/java/com/kesa/automatedselfdiagnosis/MainActivity.kt
@@ -1,33 +1,43 @@
 package com.kesa.automatedselfdiagnosis
 
-import android.app.TimePickerDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TimePicker
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-
-var alertHour:Int = 0
-var alertMin:Int = 0
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnSetTime = findViewById<Button>(R.id.setTime)
+        val picker = findViewById<TimePicker>(R.id.timePicker)
+        picker.setIs24HourView(true)
 
-        btnSetTime.setOnClickListener {
-            val timePickerDialog = TimePickerDialog(this,
-                TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
-                    alertHour = hourOfDay
-                    alertMin = minute
+        val sharedPreferences = getSharedPreferences("daily alarm", Context.MODE_PRIVATE)
+        val milis = sharedPreferences.getLong("nextNotifyTime", Calendar.getInstance().timeInMillis)
 
-                    btnSetTime.text = "${alertHour.toString()} : ${alertMin.toString()}"
-                }, 0, 0, true
-            )
-            timePickerDialog.setMessage("메시지")
-            timePickerDialog.show()
-        }
+        val nextNotifyTime = GregorianCalendar()
+        nextNotifyTime.timeInMillis = milis
+
+        val nextDate = nextNotifyTime.time
+        val dateText = SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(nextDate)
+        Toast.makeText(applicationContext,"[처음 실행시] 다음 알람은 " + dateText + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show()
+
+        val currentTime = nextNotifyTime.time
+        val hourFormat = SimpleDateFormat("kk", Locale.getDefault())
+        val minuteFormat = SimpleDateFormat("mm", Locale.getDefault())
+
+        val preHour = Integer.parseInt(hourFormat.format(currentTime))
+        val preMinute = Integer.parseInt(minuteFormat.format(currentTime))
+
+        picker.hour = preHour
+        picker.minute = preMinute
+        // Initialize TimePicker with previous settings
+
     }
 }
