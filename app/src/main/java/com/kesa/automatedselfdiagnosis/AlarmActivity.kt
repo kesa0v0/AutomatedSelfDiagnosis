4fd32fd72b79d9821fa42809c9d3356897a331e9
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-07-26 13:41:54 +0900

    testing web macro (not success)

--- app/src/main/java/com/kesa/automatedselfdiagnosis/AlarmActivity.kt
+++ app/src/main/java/com/kesa/automatedselfdiagnosis/AlarmActivity.kt
@@ -2,15 +2,15 @@ package com.kesa.automatedselfdiagnosis
 
 import android.content.Intent
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.openqa.selenium.By
-import org.openqa.selenium.Keys
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated
-import org.openqa.selenium.support.ui.WebDriverWait
+import org.openqa.selenium.chrome.ChromeOptions
 
 
 class AlarmActivity : AppCompatActivity() {
@@ -18,6 +18,9 @@ class AlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
+
+//        System.setProperty("webdriver.chrome.driver", "\bD:\\program\\selendroid\\chromedriver.exe")
+//        println("property" + System.getProperty("webdriver.chrome.driver"))
 
         val timeView = findViewById<TextView>(R.id.currentTime)
         val yes = findViewById<Button>(R.id.yesSymptom)
@@ -32,13 +35,34 @@ class AlarmActivity : AppCompatActivity() {
         }
 
         no.setOnClickListener {
-            val driver = ChromeDriver()
-            val wait = WebDriverWait(driver, 10)
-            try {
-                driver.get("https://google.com/ncr")
-            } finally {
-                driver.quit()
-            }
+            DoAsync {
+                val get = GettingStarted()
+                get.testGoogleSearch()
+            }.execute()
         }
+    }
+}
+
+class DoAsync(val handler: () -> Unit) : AsyncTask<Void, Void, Void>() {
+    override fun doInBackground(vararg params: Void?): Void? {
+        handler()
+        return null
+    }
+}
+
+class GettingStarted {
+    fun testGoogleSearch() {
+        val chromeOptions = ChromeOptions()
+        chromeOptions.setExperimentalOption("androidPackage", "com.android.chrome")
+        chromeOptions.setExperimentalOption("androidDeviceSerial", "PIXEL 3")
+        System.setProperty("webdriver.chrome.driver", "\bC:\\selendroid\\chromedriver.exe")
+        val driver: WebDriver = ChromeDriver(chromeOptions)
+        driver["http://www.google.com/"]
+        Thread.sleep(5000) // Let the user actually see something!
+        val searchBox = driver.findElement(By.name("q"))
+        searchBox.sendKeys("ChromeDriver")
+        searchBox.submit()
+        Thread.sleep(5000) // Let the user actually see something!
+        driver.quit()
     }
 }
