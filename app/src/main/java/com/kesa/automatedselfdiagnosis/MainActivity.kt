28b6662b228d4f27feff96e4002a0cde38ca2f1c
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-08-11 23:50:23 +0900

    add comments

--- app/src/main/java/com/kesa/automatedselfdiagnosis/MainActivity.kt
+++ app/src/main/java/com/kesa/automatedselfdiagnosis/MainActivity.kt
@@ -52,8 +52,11 @@ class MainActivity : AppCompatActivity() {
             getSharedPreferences("selfDiagnosis", Context.MODE_PRIVATE)
         val millis =
             sharedPreferences.getLong("nextNotifyTime", Calendar.getInstance().timeInMillis)
+        // 이전에 설정된 시간 불러오기
 
         activeSwitch.isChecked = sharedPreferences.getBoolean("switchStatus", false)
+        // 활성화 되어 있었는지 여부 불러오기
+        
         val nextNotifyTime: Calendar = GregorianCalendar()
         nextNotifyTime.timeInMillis = millis
 
@@ -72,22 +75,22 @@ class MainActivity : AppCompatActivity() {
 
         val calendar = Calendar.getInstance()
 
-        picker.setOnTimeChangedListener { _, hourOfDay, minute ->
+        picker.setOnTimeChangedListener { _, hourOfDay, minute -> // 시간선택이 변경됬을 때
             calendar.timeInMillis = System.currentTimeMillis()
             calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
             calendar.set(Calendar.MINUTE, minute)
             calendar.set(Calendar.SECOND, 0)
 
             if (calendar.before(Calendar.getInstance())) {
-                calendar.add(Calendar.DATE, 1)
+                calendar.add(Calendar.DATE, 1) // 설정된 시간이 현재 시간보다 작으면 다음날로 설정
             }
             //  Preference 에 설정한 값 저장
             val editor = getSharedPreferences("selfDiagnosis", Context.MODE_PRIVATE).edit()
             editor.putLong("nextNotifyTime", calendar.timeInMillis)
             editor.apply()
 
-            activeSwitch.isChecked = false
-            sharedPreferences.edit().putBoolean("switchStatus", false).apply()
+            activeSwitch.isChecked = false // 스위치 끄기
+            sharedPreferences.edit().putBoolean("switchStatus", false).apply() // 스위치 동기화
         }
 
 
@@ -95,15 +98,16 @@ class MainActivity : AppCompatActivity() {
         val receiver = ComponentName(this, DeviceBootReceiver::class.java)
         val alarmIntent = Intent(this, AlarmActivity::class.java)
         alarmIntent.putExtra("time", SimpleDateFormat("a hh : mm", Locale.getDefault()).format(calendar.time))
+        // 알람 설정된 시간 보내기
         val pendingIntent = PendingIntent.getActivity(this, 0, alarmIntent, 0)
 
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-        activeSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
+        activeSwitch.setOnCheckedChangeListener { _, isChecked -> 
+            if (isChecked) {  // 스위치 체크되면
                 alarmManager.setRepeating(
                     AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent
-                )
+                )   // 매일 AlarmActivity 열기
 
                 // 부팅 후 실행되는 리시버 사용가능하게 설정
                 pm.setComponentEnabledSetting(
@@ -114,14 +118,14 @@ class MainActivity : AppCompatActivity() {
 
                 Toast.makeText(this, SimpleDateFormat("a hh시 mm분으로 설정되었습니다", Locale.getDefault()).format(calendar.time), Toast.LENGTH_LONG).show()
             }
-            else {
-                alarmManager.cancel(pendingIntent)
+            else {  // 스위치 해제되면
+                alarmManager.cancel(pendingIntent) // 알람 끄기
                 Toast.makeText(this, "해제되었습니다.", Toast.LENGTH_SHORT).show()
             }
-            sharedPreferences.edit().putBoolean("switchStatus", isChecked).apply()
+            sharedPreferences.edit().putBoolean("switchStatus", isChecked).apply() // 스위치 동기화
         }
 
-
+        // 디버깅용 버튼 (알람 창 강제로 띄우기)
         val test = findViewById<Button>(R.id.test)
         test.setOnClickListener {
             startActivity(Intent(this, AlarmActivity::class.java)
